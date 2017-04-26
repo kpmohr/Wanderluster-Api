fdad9ca51b7673c8630b4ba846bde4f9e3cdadec
Kaitlyn Mohr <kpmohr@gmail.com>
2017-04-26 10:20:03 -0400

    ugh

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,23 @@
 Rails.application.routes.draw do
   devise_for :users
-  namespace :api, path: '/api'  do
+  namespace :api do
     namespace :v1 do
       scope :auth do
         get 'is_signed_in', to: 'auth#is_signed_in?'
       end
     end
   end
-  namespace :api, defaults: { format: :json }, path: '/api' do
+  namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :profiles, only: [:index, :show, :create, :destroy, :update]
     end
   end
-  namespace :api, defaults: { format: :json }, path: '/api' do
+  namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :trips, only: [:index, :show, :create, :destroy, :update]
     end
   end
-  namespace :api, defaults: { format: :json }, path: '/api' do
+  namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :maps, only: [:index, :show, :create, :destroy, :update]
     end
